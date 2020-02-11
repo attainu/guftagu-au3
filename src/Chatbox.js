74fd869717a2aefb9ce8f39884c04e1904f5e3f0
smriti-mittal-au2 <mittal.smriti001%40gmail.com>
2020-02-11 19:42:13 +0530

    chatbox bg img

---
+++ src/Chatbox.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const Chatbox = (props) => {
+    return (<div></div>)
+}
+
+
+export default Chatbox
---
+++ src/DisplayChat.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import './DisplayChat.scss';
+
+const DisplayChat = (props) => {
+    return (<div className='display-chat w-100'></div>)
+}
+
+
+export default DisplayChat
