c7d5c829fd26ee0d3d6ced277d9f7924b603312b
smriti-mittal-au2 <mittal.smriti001%40gmail.com>
2020-02-11 19:18:59 +0530

    chat content window + responsive

--- src/Home.js
+++ src/Home.js
@@ -2,21 +2,25 @@ import React, {useState, useEffect} from 'react';
 import './Home.scss';
 import Profile from './Profile';
 import SearchContacts from './SearchContacts';
-import {Route} from 'react-router-dom';
+import LandingPage from './LandingPage';
+import {Route, Redirect} from 'react-router-dom';
 
 const Home = (props) => {
+
+    const [className, setClassName] = useState('')
+
+
+    const addClass = (addClass) => {
+        setClassName(addClass)
+
+    }
+    
     return (<div className='homepage'>
-        <div className='sidebar px-0'>
+        <div className={`sidebar px-0 + ${className}`}>
             <Route exact path='/home' component={SearchContacts}></Route>
-            <Route path='/home/profile' component={Profile}></Route>     
-        </div>
-        <div className='main'>
-            <img id='phone' src='https://previews.123rf.com/images/cosmaa/cosmaa1805/cosmaa180500015/100729800-cartoon-illustration-of-phone-notification-alarm-and-police-flasher-on-phone-display-hand-holding-sm.jpg'></img>
-            <h1 className='text-secondary mt-3'>Keep your phone connected</h1>
-            <p className='text-secondary'>Talk to your friends, here, everywhere.</p>
-
-        </div>
-
+            <Route exact path='/home/profile' component={Profile}></Route>     
+        </div> 
+        <LandingPage addClass={addClass}/>           
     </div>)
 
 }
--- src/reducer/reducer.js
+++ src/reducer/reducer.js
@@ -1,10 +1,12 @@
-module.exports = (state={search:"not searched"}, action) => {
-    console.log("inside reducer: ", action.type, action.value, state)
+module.exports = (state={search:"not searched", selectedChat:null}, action) => {
+    console.log("inside reducer: ", action.type, action.value)
     switch(action.type){
         // case 'login details':
         //     return Object.assign({}, state, {login: action.value})
         case 'searched users':
                 return Object.assign({}, state, {search: action.value})
+        case "get-chat-content-for-selected-chat":
+                return Object.assign({}, state, {selectedChat: action.value})
         default:
             return state
 
